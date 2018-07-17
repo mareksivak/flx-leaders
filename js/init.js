bd8c0deaf6e540d88b720090c34cd63570a25edd
Marek <mmareq@gmail.com>
2018-07-17 15:19:56 +0800

    mobile nav

--- js/init.js
+++ js/init.js
@@ -1,11 +1,27 @@
 new WOW().init();
 
-$('.o-lazy img').hide();
+
+//$(".nav-wrapper").css("background", "yellow");
 
 //$(document).ready(function() {
 $(window).ready(function() {
-    $('.o-lazy img').show().addClass('animated800 fadeIn');
-    $('.o-lazy img').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $('.o-lazy img').removeClass('animated800 fadeIn');
+
+    console.log("ready");
+    //$('#nav-close').addClass("AAA");
+    $(".nav-wrapper","#nav-close").css("background-color", "yellow");
+    
+    //open mobile nav
+    $("#nav-open").click(function() {
+        console.log("open");
+        $(".mobile-nav").show();
+    });
+    
+    //close mobile nav
+    $("#nav-close,.mobile-nav .menu,.nav-wrapper").click(function() {
+        console.log("close");
+        $(".mobile-nav").hide();
     });
 });
+
+console.log("init");
+
